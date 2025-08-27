428118fb362a09e7e33778a513f6fdfa217bed06
nmkzzztos <nmkzzztos@users.noreply.github.com>
2025-08-27 15:29:44 +0000

    deploy: de7644104de94f34290da8e80b62643183713b6e

---
+++ assets/js/689d0b30.bdf6246b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[3657],{4058:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/Drafto-docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.432593eb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/Drafto-docs/blog","blogTitle":"Blog","authorsListPath":"/Drafto-docs/blog/authors"}')}}]);
